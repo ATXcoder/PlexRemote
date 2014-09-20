e7c5da011f8c65575f23bc7e95482142c9339e9c
Thomas Sloan <tsloan82@gmail.com>
2014-09-20 11:06:58 -0500

    Initial commit

---
+++ app/src/main/java/net/home/apps/plexremote/api.java
@@ -0,0 +1,61 @@
+package net.home.apps.plexremote;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by thomas on 9/13/14.
+ */
+public class api {
+
+    public api(String api_url)
+    {
+        api_call apiCall = new api_call();
+        apiCall.execute(api_url);
+    }
+
+    private class api_call extends AsyncTask <String, Void, String>
+    {
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(strings[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    out.close();
+                    responseString = out.toString();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return responseString;
+        }
+    }
+}
---
+++ app/src/main/java/net/home/apps/plexremote/remote.java
@@ -0,0 +1,190 @@
+package net.home.apps.plexremote;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class remote extends Activity {
+
+    // Volume
+    private static int vol = 80;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote);
+
+        // Get buttons
+        ImageView bttn_left = (ImageView)findViewById(R.id.bttn_left);
+        ImageView bttn_right = (ImageView)findViewById(R.id.bttn_right);
+        ImageView bttn_up = (ImageView)findViewById(R.id.bttn_top);
+        ImageView bttn_down = (ImageView)findViewById(R.id.bttn_down);
+        ImageView bttn_ok = (ImageView)findViewById(R.id.bttn_OK);
+        ImageView bttn_back = (ImageView)findViewById(R.id.bttn_back);
+
+        // Player buttons
+        ImageView bttn_rewind = (ImageView)findViewById(R.id.bttn_rewind);
+        ImageView bttn_play = (ImageView)findViewById(R.id.bttn_play);
+        ImageView bttn_pause = (ImageView)findViewById(R.id.bttn_pause);
+        ImageView bttn_fastforward = (ImageView)findViewById(R.id.bttn_fastforward);
+
+
+        //Click Left Button
+        bttn_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                api_prep("moveLeft", "navigation");
+            }
+        });
+
+        bttn_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Right button pushed
+                api_prep("moveRight", "navigation");
+            }
+        });
+
+        bttn_down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Down button pushed
+                api_prep("moveDown", "navigation");
+            }
+        });
+
+        bttn_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Up button pushed
+                api_prep("moveUp", "navigation");
+            }
+        });
+
+        bttn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // OK button pushed
+                api_prep("select", "navigation");
+            }
+        });
+
+        bttn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                api_prep("back", "navigation");
+            }
+        });
+
+        // Player Controls
+        bttn_rewind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                api_prep("stepback", "playback");
+            }
+        });
+
+        bttn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                api_prep("play", "playback");
+            }
+        });
+
+        bttn_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                api_prep("pause", "playback");
+            }
+        });
+
+        bttn_fastforward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                api_prep("stepforward", "playback");
+            }
+        });
+    }
+
+
+    /* DOSN'T WORK
+    // Save changes on rotation
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("vol", String.valueOf(vol));
+    }
+    */
+
+    // Capture Hardware Volume Controller
+    // We'll use the volume rocker to increase and decrease volume
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        int action = event.getAction();
+        Log.i("Plex Remote", "Volume " + keyCode + " " + action);
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    if(vol > 0 && vol < 100)
+                    {
+                        vol = vol + 5;
+                    }
+
+                    Log.i("Plex Remote","Volume = " + String.valueOf(vol));
+                    api_prep("setParameters?type=video&volume=" + String.valueOf(vol), "playback");
+                }
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    if(vol > 0 && vol < 100)
+                    {
+                        vol = vol - 5;
+                    }
+                    Log.i("Plex Remote","Volume = " + String.valueOf(vol));
+                    api_prep("setParameters?type=video&volume=" + String.valueOf(vol), "playback");
+                }
+                return true;
+            default:
+                return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    private void api_prep(String api_method, String api_controller)
+    {
+        // Get the prefs --later
+        String server = "192.168.1.10";
+        String port = "3005";
+        String client = "192.168.10.1";
+
+        //String api_url = "http://" + server +":" + port + "/system/players/" + client + "/" + api_controller + "/" + api_method;
+        String api_url = "http://" + server + ":" + port + "/player/" + api_controller + "/" + api_method;
+        Log.i("Plex Remote", api_url);
+        new api(api_url);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.remote, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
